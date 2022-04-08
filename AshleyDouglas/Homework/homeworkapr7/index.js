06d8a0be4493e9510d069ba09413165a763d39e1
Yomicd <99566327+Yomicd@users.noreply.github.com>
2022-04-08 09:46:34 -0400

    Add files via upload
    Javascript and for loops practice/ Ramen website

---
+++ AshleyDouglas/Homework/homeworkapr7/index.js
@@ -0,0 +1,30 @@
+
+
+
+
+let Shoyu = ["chicken broth", "noodles", "fish", "egg"]
+let Miso = ["miso broth", "noodles", "egg", "tofu"]
+let Tonkotsu = ["pork bone broth", "noodles", "pork belly", "egg"];
+
+
+let RamenType = ["Shoyu", "Miso", "Tonkotsu"]
+
+    
+let ingredient = ["extra noodles", "egg", "tofu", "fish", "pork belly"];
+
+let cart = 0;
+
+for ( RamenType + ingredient; cart = RamenType + ingredient[1];);
+
+if ( ingredient ="extra noodles", "egg", "tofu", "fish", "pork belly"){
+    console.log(ingredient++)
+};
+
+console.log(RamenType);
+console.log(RamenType[1]);
+
+
+
+
+
+
