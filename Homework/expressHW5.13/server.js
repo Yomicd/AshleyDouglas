2f9c28a4f956571315ea1d12578edae7fb459853
Ashley Douglas <douglasashleyc@gmail.com>
2022-05-13 15:43:18 -0400

    Tip Calculator express application created

--- Homework/expressHW5.13/server.js
+++ Homework/expressHW5.13/server.js
@@ -1,16 +1,19 @@
-
+//------------Greeting--------------
 const express = require('express')
-
 const app = express()
 
-const name = "Yomi"
+// const name = "Yomi"
 
+// app.get('/greeting/:name', (req, res)=>{
+//     res.send('Hey now ' + req.params.name)
+// })
 
-
-//
-
-app.get('/greeting/:name', (req, res)=>{
-    res.send('Hey now ' + req.params.name)
+app.get('/tip/:total/:tipPercentage', (req, res)=>{
+    let total = Number(req.params.total)
+    let tipPercentage =Number(req.params.tipPercentage);
+    let tip = total * (tipPercentage/100) 
+    tip = String(tip)
+    res.send(tip)
 })
 
 
