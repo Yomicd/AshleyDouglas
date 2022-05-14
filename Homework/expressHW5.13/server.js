abe24c70abd0d6b869af779f44078991e7a9db50
Ashley Douglas <douglasashleyc@gmail.com>
2022-05-14 19:49:49 -0400

    attempt at Take one Down Pass it Around, in review

--- Homework/expressHW5.13/server.js
+++ Homework/expressHW5.13/server.js
@@ -1,30 +1,32 @@
 //------------Greeting--------------
+const { application } = require('express')
 const express = require('express')
 const app = express()
 
 
-responses = [
-    "It is certain", 
-    "It is decidedly so", 
-    "Without a doubt", "Yes definitely",
-    "You may rely on it", "As I see it yes", 
-    "Most likely", 
-    "Outlook good",
-    "Yes", 
-    "Signs point to yes", 
-    "Reply hazy try again", 
-    "Ask again later",
-    "Better not tell you now", 
-    "Cannot predict now", 
-    "Concentrate and ask again",
-    "Don't count on it", 
-    "My reply is no", 
-    "My sources say no",
-    "Outlook not so good", 
-    "Very doubtful"
-]
 
-let Ballresponse = responses[Math.floor(Math.random() * responses.length)]
+// responses = [
+//     "It is certain", 
+//     "It is decidedly so", 
+//     "Without a doubt", "Yes definitely",
+//     "You may rely on it", "As I see it yes", 
+//     "Most likely", 
+//     "Outlook good",
+//     "Yes", 
+//     "Signs point to yes", 
+//     "Reply hazy try again", 
+//     "Ask again later",
+//     "Better not tell you now", 
+//     "Cannot predict now", 
+//     "Concentrate and ask again",
+//     "Don't count on it", 
+//     "My reply is no", 
+//     "My sources say no",
+//     "Outlook not so good", 
+//     "Very doubtful"
+// ]
+
+// let Ballresponse = responses[Math.floor(Math.random() * responses.length)]
 
 // const name = "Yomi"
 
@@ -41,14 +43,28 @@ let Ballresponse = responses[Math.floor(Math.random() * responses.length)]
 // })
 
 //------------------Magic 8 Ball--------------------
-app.get('/magic/Will%20I%20Be%20A%20Millionaire', (req, res)=>{
-    if(responses[req.params.Ballresponse]){
+// app.get('/magic/Will%20I%20Be%20A%20Millionaire', (req, res)=>{
+//     if(responses[req.params.Ballresponse]){
 
-        res.send('<h1>Will I become a millionaire </h1>' +  req.params.Ballresponse)
-    };
+//         res.send('<h1>Will I become a millionaire </h1>' +  req.params.Ballresponse)
+//     };
 
+// })
+
+
+///----------------------Take One Down Pass It Around---------------
+
+const bottlesOfBeer = () => function range(start, end){
+    for (let n = start; n => end; n--){
+        // yield n;
+    }
+    for (n of range(99,0)){
+    app.get('/:number_of_bottles', (req, res)=>{
+    res.send(req.params.bottlesOfBeer + 'Bottles of beer on the wall')
 })
+}}
 
+//-------does not work, need assistance, will come back 
 
 app.listen(3000, function(){
     console.log("listen on port 3000")
