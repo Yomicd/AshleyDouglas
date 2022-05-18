4eab7adc7b1e12ec7f4442a31fc1e7da2a4458b4
Ashley Douglas <douglasashleyc@gmail.com>
2022-05-18 01:00:20 -0400

    Added dynamic anchor tags to index.jsx and Created show views of each pokemon

--- Homework/pokemon_app/views/Index.jsx
+++ Homework/pokemon_app/views/Index.jsx
@@ -1,12 +1,9 @@
 const React = require('react');
 const pokemon = require('../models/pokemon');
 
-// function capitalize(str){
-//     return str.charAt(0).toUpperCase() + str.slice(1);
-// }
-
-
-
+function capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
 class Index extends React.Component {
     render(){
         return (
@@ -21,6 +18,7 @@ class Index extends React.Component {
                            this.props.pokemon.map((pokemon, i)=>{
                                return (
                                    <li>
+                                       <a href={`/pokemon/${i}`}>{capitalize(pokemon.name)}</a>{' '} I Choose You!
 
                                    </li>
                                )
