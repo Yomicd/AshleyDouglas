5fefeaa9ab81ff1379e2fc097b6d7f4b66fd0dae
Ashley Douglas <douglasashleyc@gmail.com>
2022-05-18 00:38:19 -0400

    Show view shows req.params.id

--- Homework/pokemon_app/views/Index.jsx
+++ Homework/pokemon_app/views/Index.jsx
@@ -4,17 +4,34 @@ const pokemon = require('../models/pokemon');
 // function capitalize(str){
 //     return str.charAt(0).toUpperCase() + str.slice(1);
 // }
+
+
+
 class Index extends React.Component {
     render(){
         return (
             <div>
-                <h1>See All The Pokemon</h1>
+                
                 <div style={myStyle}> 
-      
-      
-      
-      
-      <h1>My First React Component!</h1>
+                <h1>See All The Pokemon</h1>
+
+                
+                    <ul>
+                       {
+                           this.props.pokemon.map((pokemon, i)=>{
+                               return (
+                                   <li>
+
+                                   </li>
+                               )
+                           })
+                       }
+         
+                    </ul>
+               
+
+        
+    
                
             </div>
             </div>
