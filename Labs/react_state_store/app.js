ab59e9a53a238a8797b4818e8579d11ce4995e2f
Ashley Douglas <douglasashleyc@gmail.com>
2022-06-08 14:06:26 -0400

    render items to shopping list

--- Labs/react_state_store/app.js
+++ Labs/react_state_store/app.js
@@ -1,143 +1,199 @@
-//create class component
-class ProductList extends React.Component {
+// CREATE CLASS COMPONENT
+// class ProductList extends React.Component {
+//   state = {
+//     inShoppingCart: false,
+//   };
+
+//   // Create a function to toggle the state of our shopping cart
+//   handleCartToggle = (e) => {
+//     // this.setState({
+//     //   inShoppingCart: !this.state.inShoppingCart,
+//     // });
+//     console.log(e.target);
+//   };
+
+//   render() {
+//     // Render the product list
+// const productList = this.props.data.map((element) => {
+//   return (
+//     <li onClick={this.handleCartToggle}>
+//       {element.name} {element.price}{" "}
+//       {this.state.inShoppingCart ? <span>in shopping cart</span> : ""}
+//     </li>
+//   );
+// });
+// return <ul>{productList}</ul>;
+//   }
+// }
+
+//create shopping list component to recieve the datat from the cart
+class ShoppingList extends React.Component{
     render(){
-        //Render the productlist
-        //the data array that we map
-        const productList = this.props.data.map((element)=>{
-            return (
+        const cartItems = this.props.cart.map((element)=>{
+            return(
                 <li>
                     {element.name} {element.price}
                 </li>
-
-            );
-        });
+            )
+        })
+    
         return(
             <ul>
-                {productList}
+                {cartItems}
             </ul>
         )
     }
 }
 
-// Create calss componnt to render data to the screen
-class App extends React.Component{
-    //initialize your state
+class ProductList extends React.Component {
     state = {
-        data: data,
-        value: "",
-        price: 0,
-        description: 'Describe this item'
+      inShoppingCart: false,
     };
-    // creat a method to change user input
-    handleChange = (e) =>{
-        this.setState({
-            [e.target.id]: e.target.value,
-
-        });
+  
+    handleCartToggle = () => {
+      this.setState({
+        inShoppingCart: !this.state.inShoppingCart,
+      });
     };
-
-    // create method to submit the form
-    handleFormSubmit = (e) =>{
-    e.preventDefault();
-   
-
-    // add new item to our array
-    const newItem = {
+    render() {
+      // console.log(this.props.handleShoppingCart);
+      return (
+        <li
+          onClick={() => {
+            return this.props.handleShoppingCart(this.props.element);
+          }}
+        >
+          {this.props.element.name}{" "}
+          {this.state.inShoppingCart ? <span>in Shopping Cart</span> : null}
+        </li>
+      );
+    }
+  }
+  
+  /// CREATE CLASS COMPONENT TO RENDER DATA TO THE SCREEN
+  class App extends React.Component {
+    // INITIALIZE YOUR STATE
+    state = {
+      data: data,
+      value: "",
+      name: "",
+      price: 0,
+      description: "Describe this item",
+      isHiring: true,
+      cart: [],
+    };
+  
+    // CREATE A METHOD TO CHANGE USER INPUT
+    handleChange = (e) => {
+      this.setState({
+        [e.target.id]: e.target.value,
+      });
+    };
+  
+    // Create a method to submit the form
+    handleFormSubmit = (e) => {
+      e.preventDefault();
+      // Lets add a new item to our data array
+      const newItem = {
         name: this.state.name,
         price: this.state.price,
         description: this.state.description,
-    };
-    
-     // add the new item to our data array
-     this.setState({
+      };
+  
+      // Add the new item to our data array
+      this.setState({
         data: [newItem, ...this.state.data],
-    });
-    
-    console.log(this.state.data);
-};
-
-//create a function to toggle thevalue of our hiring state
-handleToggleHiring = () =>{
-    this.setState({
-        isHiring: !this.state.isHiring,
-    })
-}
-
-
-    render(){
-        const dataList = this.state.data.map((element)=>{
-            return(
-                <ul>
-                    <li>{element.name } {element.price}</li>
-                </ul>
-            )
-        });
-        return(
-            <div>
-                <h1>Shopping Gal</h1>
-
-                <h2>
-                    {this.state.isHiring ? 
-                        <h2>Yes we are hiring</h2> 
-                        :
-            
-                         <h2>Sorry try again tomorrow</h2>
-                         }
-                        
-                    
-                </h2>
-                <button onClick={this.handleToggleHiring}>Toggle Hiring</button>
-                <form onSubmit={this.handleFormSubmit}>
-                
-                <label htmlFor="name"></label>
-                <input 
-                id="name"
-                type="text" 
-                value={this.state.name} 
-                onChange={this.handleChange} 
-                />
-                <br />
-                <br />
-
-                <label htmlFor="price"></label>
-                <input 
-                id="price"
-                type="number" 
-                value={this.state.price} 
-                onChange={this.handleChange} 
-                />
-                <br />
-                <br />
-
-                <label htmlFor="description"></label>
-                <input 
-                id="description"
-                type="text" 
-                value={this.state.description} 
-                onChange={this.handleChange} 
-                />
-                <br />
-                <br />
-                <input type="submit" />
-                </form>
-                <div>
-                    <h2>Preview our new item</h2>
-                    <h3>{this.state.name}</h3>
-                    
-                    <h3>{this.state.price}</h3>
-                   
-                    <h3>{this.state.description}</h3>
-                </div>
-            
-               
-                {/* {dataList} */}
-                {/* Passing props from app component to product
-                list component */}
-                <ProductList data={data} />
-            </div>
-        );
+      });
     };
-}
-
-//Render component to the screen
-ReactDOM.render(<App/>, document.querySelector(".container"))
+  
+    // CREATE A FUNCTION TO TOGGLE THE VALUE OF OUR HIRING STATE
+    handleToggleHiring = () => {
+      this.setState({
+        isHiring: !this.state.isHiring,
+      });
+    };
+  
+    /// Step 1. Create a method in App component that keeps a handle inside Product list component
+    handleShoppingCart = (item) => {
+      this.setState({
+          cart: [item, ... this.state.cart],
+      })
+    };
+  
+    render() {
+      console.log(this.state.cart);
+      const dataList = this.state.data.map((element) => {
+        return (
+          //
+          <ul className="products">
+            {/* <li>
+              {element.name} {element.price}
+            </li> */}
+            {/* Step 2. Hook our handle to product list component */}
+            <ProductList
+              element={element}
+              handleShoppingCart={this.handleShoppingCart}
+            />
+          </ul>
+        );
+      });
+      return (
+        <div>
+          <h1>Big Time Shopping</h1>
+          <h2>
+            {this.state.isHiring ? (
+              <h2>Yes we are hiring</h2>
+            ) : (
+              <h2>Sorry try again tomorrow 🥲</h2>
+            )}
+          </h2>
+          <button onClick={this.handleToggleHiring}>Toggle Hiring</button>
+          <form onSubmit={this.handleFormSubmit}>
+            <label htmlFor="name">Name: </label>
+            <input
+              id="name"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+            <br />
+            <br />
+            <label htmlFor="price">Price: </label>
+            <input
+              id="price"
+              type="text"
+              value={this.state.price}
+              onChange={this.handleChange}
+            />
+            <br />
+            <br />
+            <label htmlFor="description">Description: </label>
+            <input
+              id="description"
+              type="text"
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
+            <br />
+            <br />
+            <input type="submit" />
+          </form>
+          <div className="preview">
+            <h2>Preview our new item</h2>
+            <h3>{this.state.name}</h3>
+            <h4>{this.state.price}</h4>
+            <h5>{this.state.description}</h5>
+          </div>
+          {dataList}
+          <div className="cart">
+            <h3> Shopping Cart </h3>
+            <ul></ul>
+          </div>
+          <ShoppingList cart={this.state.cart} />
+        </div>
+      );
+    }
+  }
+  
+  // RENDER COMPONENT TO THE SCREEN
+  ReactDOM.render(<App />, document.querySelector(".container"));
