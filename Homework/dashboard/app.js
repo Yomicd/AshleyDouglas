21292d92bf0d4ae5b38e505be01bce511418df15
Ashley Douglas <douglasashleyc@gmail.com>
2022-06-13 22:25:08 -0400

    added styling to UI

--- Homework/dashboard/app.js
+++ Homework/dashboard/app.js
@@ -1,11 +1,26 @@
+const h3style = {
+    fontSize: "20px",
+    color: "purple",
+    textAlign: "center"
+}
+const h4style = {
+    color: "white",
+    textAlign: "center"
+}
+const h5style = {
+    color: "blue",
+    textAlign: "center"
+}
 
 const appstyle = {
     border: "black solid",
     height: "100vh",
     display: "flex",  
     margin: "auto",
-    marginLeft: "0"
-
+    marginLeft: "0",
+    background: "rgb(238,174,202)",
+    background: "radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)"
+    
 
 }
 const sidePanelstyle = {
@@ -15,6 +30,12 @@ const sidePanelstyle = {
     maxWidth:"20%",
     marginLeft: "5px",
     height: "40vh",
+    background: "rgba( 57, 26, 70, 0.25 )",
+    boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+    backdropFilter:" blur( 4px )",
+    webkitBackdropFilter:" blur( 4px )",
+    borderRadius: "10px",
+    border: "1px solid rgba( 255, 255, 255, 0.18 )",
     // marginBottom: "0"
 
 }
@@ -30,7 +51,12 @@ const babyBoxStyle = {
     justifyContent: "",
     wordWrap:"break-word",
     margin: "auto",
-    marginRight: "10px"
+    marginRight: "10px",
+    boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+    backdropFilter:" blur( 4px )",
+    webkitBackdropFilter:" blur( 4px )",
+    borderRadius: "10px",
+    border: "1px solid rgba( 255, 255, 255, 0.18 )",
     
 }
 const recContainer = {
@@ -44,7 +70,12 @@ const rectangleStyle = {
     flexDirection: "column",
     margin: "0",
     marginTop:"5px",
-    alignItems: "center"
+    alignItems: "center",
+    boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+    backdropFilter:" blur( 4px )",
+    webkitBackdropFilter:" blur( 4px )",
+    borderRadius: "10px",
+    border: "1px solid rgba( 255, 255, 255, 0.18 )",
     
 }
 const greyinnerbox = {
@@ -63,10 +94,8 @@ const Wrapperstyle = {
     justifyContent: "space-between",
     padding:" 0",
     flexFLow: "row wrap",
-    margin: "auto"
-
-
-
+    margin: "auto",
+    height: "550px"
 }
 
 class Wrapper extends React.Component {
@@ -84,15 +113,15 @@ class SidePanel extends React.Component {
     render (){
         return(
             <div style={sidePanelstyle}>
-               <h1>
+               <h3 style={h3style}>
                    Long box add style
-               </h1>
-               <h5>Dashboard</h5>
-               <h5>Widget</h5>
-               <h5>Reviews</h5>
-               <h5>Customers</h5>
-               <h5>Online Analysis</h5>
-               <h5>Settings</h5>
+               </h3>
+               <h5 style={h5style}>Dashboard</h5>
+               <h5 style={h5style}>Widget</h5>
+               <h5 style={h5style}>Reviews</h5>
+               <h5 style={h5style}>Customers</h5>
+               <h5 style={h5style}>Online Analysis</h5>
+               <h5 style={h5style}>Settings</h5>
 
 
             </div>
@@ -105,18 +134,18 @@ class BoxWidget extends React.Component {
         return(
             <div style={widgetflex}>
             <div style={babyBoxStyle} >
-                <h3>BOX 1 add style </h3>
-                <h5>Reviews</h5>
+                <h3 style={h3style}>BOX 1 add style </h3>
+                <h5 style={h5style}>Reviews</h5>
                 <h4>1,281</h4>
             </div>
             <div style={babyBoxStyle}>
-                <h3>BOX 2 add style</h3>
-                <h5>Average Rating</h5>
+                <h3 style={h3style}>BOX 2 add style</h3>
+                <h5 style={h5style}>Average Rating</h5>
                 <h4>4.6</h4>
             </div>
             <div style={babyBoxStyle}>
-                <h3>BOX 3 add style</h3>
-                <h5>Sentiment Analysis</h5>
+                <h3 style={h3style}>BOX 3 add style</h3>
+                <h5 style={h5style}>Sentiment Analysis</h5>
                 <h4>960</h4>
                 <h4>122</h4>
                 <h4>321</h4>
@@ -156,6 +185,7 @@ class RectangleBox extends React.Component {
     render(){
         return(
             <div style={appstyle}>
+                
                 <Wrapper>
                 <SidePanel> 
                 </SidePanel>
